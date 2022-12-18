7b60bfafaf4794179f99de61ea4ae767fec5a5af
aya mabaouj <mabaoujaya@gmail.com>
2022-12-18 12:09:32 +0100

    first commit

---
+++ src/components/RiverInformation/RiverInformation.js
@@ -0,0 +1,29 @@
+import React,{lazy, Suspense, useEffect,useState} from 'react';
+import PropTypes from 'prop-types';
+import { getRiverInformation } from '../../services/rivers';
+
+export default function RiverInformation({ name }) {
+    const [riverInformation, setRiverInformation] = useState({});
+    useEffect(()=>{
+        let mounted = true;
+    getRiverInformation(name).then(data=>{
+        if(mounted){
+        setRiverInformation(data)}
+        });
+        return () => {mounted = false}
+    }, [name])
+    return(
+    <div>
+      <h2>River Information</h2>
+      <ul>
+      <li>Continent: {riverInformation?.continent}</li>
+        <li>Length: {riverInformation?.length}</li>
+        <li>Outflow: {riverInformation?.outflow}</li>
+      </ul>
+    </div>
+  )
+}
+
+RiverInformation.protoType = {
+    name:PropTypes.string.isRequired
+}
